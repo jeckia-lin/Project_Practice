e45aa9beff9ed6d9ee4fd040dbe5b4b78b6583f9
林跃凯 <linyuekai@linyuekais-MacBook-Pro.local>
2022-03-20 12:19:51 +0800

    add all file

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Person ming = new Person();
+        ming.setName("小明");
+        ming.setAge(12);
+        System.out.println(ming.getAge());
+    }
+}
+class Person {
+    private String name;
+    private int age;
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
